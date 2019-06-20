a400e03fe6cf57c83d5ce3228e81d520872576c5
eurobin4321 <eurobin4321@gmail.com>
2019-06-20 19:13:44 +0900

    chore: 📦 Landing page routing 및 jade → ejs

--- routes/v1/main/index.js
+++ routes/v1/main/index.js
@@ -2,4 +2,10 @@ import { Router } from 'express';
 
 const router = new Router();
 
+router.get('/', function(req,res,next){
+    res.render('index', {
+        "title" : "피'플"
+    });
+});
+
 export default router;
