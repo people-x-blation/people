393ab4548d7c1f04e948e21ea7a51be2d65517c3
eurobin4321 <eurobin4321@gmail.com>
2019-06-21 00:13:29 +0900

    feat: ⚡ Landing Page outline

---
+++ public/javascript/base.js
@@ -0,0 +1,17 @@
+window.onload = function(){
+    fadeOutEffect("loading");
+}
+
+function fadeOutEffect(target) {
+    var fadeTarget = document.getElementById(target);
+    var fadeEffect = setInterval(function () {
+        if (!fadeTarget.style.opacity) {
+            fadeTarget.style.opacity = 1;
+        }
+        if (fadeTarget.style.opacity > 0) {
+            fadeTarget.style.opacity -= 0.1;
+        } else {
+            clearInterval(fadeEffect);
+        }
+    }, 100);
+}
