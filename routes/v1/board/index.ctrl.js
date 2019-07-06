44a7e88f49b5c4d0c0754ff3c6ead7fe912f8351
eurobin4321 <eurobin4321@gmail.com>
2019-07-06 23:40:40 +0900

    chore: 📦 brief scaffold

--- routes/v1/board/index.ctrl.js
+++ routes/v1/board/index.ctrl.js
@@ -5,3 +5,11 @@ export const list = (req,res) => {
 export const read = (req,res) => {
     res.render("board/read");
 }
+
+export const search = (req,res) => {
+    res.render("board/search");
+}
+
+export const write = (req,res) => {
+    res.render("board/write");
+}
--- routes/v1/board/index.js
+++ routes/v1/board/index.js
@@ -4,10 +4,14 @@ const router = new Router();
 
 import { 
     list,
-    read
-     } from './index.ctrl';
+    read,
+    search,
+    write
+    } from './index.ctrl';
 
 router.get('/', list);
 router.get('/read', read);
+router.get('/search', search);
+router.get('/write', write);
 
 export default router;
