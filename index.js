64b4a7d9968db97c2add2bb2a79e12a32893d7a6
Q00 <jqyu.lee@gmail.com>
2019-08-06 13:46:28 +0900

    chore: 📦 add module resolver

--- index.js
+++ index.js
@@ -22,7 +22,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 // jQuery 지정
-app.use('/jquery',express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.use(
+  '/jquery',
+  express.static(path.join(__dirname, '/node_modules/jquery/dist')),
+);
 app.use(bodyParser.json());
 app.use('/', router);
 
