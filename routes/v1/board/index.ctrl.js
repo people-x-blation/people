f84361420233664cfd24c05cb7b9f73f3df90b77
eurobin4321 <eurobin4321@gmail.com>
2019-08-09 22:27:27 +0900

    feat: ⚡ board render

--- routes/v1/board/index.ctrl.js
+++ routes/v1/board/index.ctrl.js
@@ -1,13 +1,81 @@
-import { select, insert, update } from '~/db/query';
+import {
+  select,
+  insert,
+  update
+} from '~/db/query';
+
+// 지역 매핑용
+let locationTable = {
+  "1": "서울",
+  "2": "부산",
+  "3": "대구",
+  "4": "인천",
+  "5": "대전",
+  "6": "광주",
+  "7": "울산",
+  "8": "세종",
+  "9": "강원도",
+  "10": "경기도",
+  "11": "충청남도",
+  "12": "전라북도",
+  "13": "전라남도",
+  "14": "경상북도",
+  "15": "경상남도",
+  "16": "제주도"
+}
+
 export const list = async (req, res) => {
+  let userInfo = {};
   try {
-    const result = await select('*', 'board', 'TRUE');
-    console.log(result.rows);
-    res.render('board/list', result.rows);
+    const list = await select('*', 'board', `locations = '${req.params.location}'`);
+    for (let row in list.rows) {
+      // 임시 변수
+      let memberInfo = await select('usernum, nickname, blood, profile', 'member', `usernum = ${list.rows[row].author}`);
+      memberInfo = memberInfo.rows[0];
+      userInfo[list.rows[row].author] = {
+        "nicknmemberInfoame": memberInfo.nickname,
+        "blood": memberInfo.blood,
+        "profile": memberInfo.profile,
+      }
+    }
+    console.log(list.rows);
+    res.render('board/list', {
+      list: list.rows,
+      user: userInfo,
+      location: locationTable[req.params.location],
+    });
   } catch (err) {
     console.log(err);
   }
 };
+
+export const listAll = async (req, res) => {
+  let userInfo = {};
+  try {
+    console.log(typeof req.params.location);
+    const list = await select('*', 'board', 'TRUE');
+    for (let row in list.rows) {
+      // 임시 변수
+      let memberInfo = await select('usernum, nickname, blood, profile', 'member', `usernum = ${list.rows[row].author}`);
+      memberInfo = memberInfo.rows[0];
+      userInfo[list.rows[row].author] = {
+        "nicknmemberInfoame": memberInfo.nickname,
+        "blood": memberInfo.blood,
+        "profile": memberInfo.profile,
+      }
+    }
+    console.log(list.rows);
+    res.render('board/list', {
+      list: list.rows,
+      user: userInfo,
+      location: locationTable
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+
 
 export const read = (req, res) => {
   res.render('board/read');
--- routes/v1/board/index.js
+++ routes/v1/board/index.js
@@ -4,12 +4,14 @@ const router = new Router();
 
 import { 
     list,
+    listAll,
     read,
     search,
     write
     } from './index.ctrl';
 
-router.get('/', list);
+router.get('/', listAll);
+router.get('/:location', list);
 router.get('/read', read);
 router.get('/search', search);
 router.get('/write', write);
