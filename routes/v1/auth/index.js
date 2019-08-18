4f35a24d0cd989eb7811639f3d4fe630ec0e2f84
Q00 <jqyu.lee@gmail.com>
2019-08-19 04:08:18 +0900

    refacor: 🛠 카카오 로그인시 회원가입 처리

--- routes/v1/auth/index.js
+++ routes/v1/auth/index.js
@@ -1,9 +1,6 @@
 import { Router } from 'express';
 import passport from 'passport';
-import { kakao } from './index.ctrl';
 
 const router = new Router();
-
-router.get('/kakao', passport.authenticate('kakao'), kakao);
 
 export default router;
