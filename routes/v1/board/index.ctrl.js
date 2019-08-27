f4904b8a43330ce6be3c74f964ae49757856bcad
Q00 <jqyu.lee@gmail.com>
2019-08-28 07:13:32 +0900

    feat: ⚡️ read modue 추가

--- routes/v1/board/index.ctrl.js
+++ routes/v1/board/index.ctrl.js
@@ -1,5 +1,5 @@
 import { select, insert, update, findName } from '~/db/query';
-
+import { board, comment } from '~/db/model';
 // 지역 매핑용
 const locationTable = {
   '1': '서울',
@@ -35,6 +35,7 @@ export const boardlist = async (req, res) => {
       '*',
       'board',
       query,
+      '',
       `order by boardnum desc limit ${size} offset ${begin}`,
     );
 
@@ -48,30 +49,32 @@ export const boardlist = async (req, res) => {
 
       let content = item.contents;
 
-      if (item.contents.length > 25) { content = content.substring(0, 24) + '...'; }
+      if (item.contents.length > 25) {
+        content = content.substring(0, 24) + '...';
+      }
 
       memberInfo = memberInfo.rows[0];
-      
-      const board = {
-        boardnum: item.boardnum,
-        title: item.title,
-        like_count: item.like_count,
-        created_at: item.created_at,
-        location: item.locations,
-        hospital: item.hospital,
-        content: content,
-        nickname: memberInfo.nickname,
-        blood: memberInfo.blood,
-      };
-
-      boardList.push(board);
+      const board_object = Object.create(board);
+      board_object.boardnum = item.boardnum;
+      board_object.title = item.title;
+      board_object.like_count = item.like_count;
+      board_object.created_at = item.created_at;
+      board_object.location = item.locations;
+      board_object.hospital = item.hospital;
+      board_object.content = content;
+      board_object.nickname = memberInfo.nickname;
+      board_object.blood = memberInfo.blood;
+      boardList.push(board_object);
     }
-    
+
     if (page == 1) {
       res.render('board/list', {
         list: boardList,
-        location: (typeof locationTable[req.params.location] === 'undefined') ? 0 : locationTable[req.params.location],
-        locationTable: locationTable
+        location:
+          typeof locationTable[req.params.location] === 'undefined'
+            ? 0
+            : locationTable[req.params.location],
+        locationTable: locationTable,
       });
     } else {
       res.json(boardList);
@@ -82,21 +85,42 @@ export const boardlist = async (req, res) => {
 };
 
 export const read = async (req, res) => {
-  
   const boardnum = req.params.boardnum;
-  try{
+  console.log(boardnum);
+  try {
     const article = await select(
-      '*',
-      'board',
-      `boardnum = ${boardnum}`
+      'b.boardnum, b.title, b.like_count, b.create_at, b.show_flag, b.locations, b.hospital, b.contents, u.nickname, r.commentnum,q.nickname,r.contents',
+      'board as b',
+      `b.boardnum = ${boardnum}`,
+      'join member as u on b.author = u.usernum left join comment as r using(boardnum) left join member as q on r.usernum= q.usernum',
     );
-    const nickname = await findMember(article.rows[0].author);
-    const articleTable = {
-      nickname : nickname,
-      article: article.rows[0]
+    console.log(article);
+    const detail = article.rows[0];
+    const board_object = Object.create(board);
+    board_object.boardnum = detail.boardnum;
+    board_object.title = detail.title;
+    board_object.like_count = detail.like_count;
+    board_object.created_at = detail.created_at;
+    board_object.location = detail.locations;
+    board_object.hospital = detail.hospital;
+    board_object.content = detail.content;
+    board_object.nickname = detail.nickname;
+    board_object.blood = detail.blood;
+    const comments = [];
+
+    for (let item of article.rows) {
+      const repl = Object.create(comment);
+      repl.comment_num = item.commentnum;
+      repl.conetnt = item.contents;
+      comments.push(repl);
     }
+    const articleTable = {
+      board: board_object,
+      reply: comments,
+    };
+    console.log(articleTable);
     res.render('board/read', articleTable);
-  }catch(e){
+  } catch (e) {
     console.log(e);
   }
 };
@@ -108,4 +132,3 @@ export const search = (req, res) => {
 export const write = (req, res) => {
   res.render('board/write');
 };
-
