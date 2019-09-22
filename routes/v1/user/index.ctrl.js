986d31734c886ffee1c56a63cd69e4341c2c3dea
eurobin4321 <eurobin4321@gmail.com>
2019-09-22 22:22:45 +0900

    fix: 🔥 nav / router fix

--- routes/v1/user/index.ctrl.js
+++ routes/v1/user/index.ctrl.js
@@ -1,8 +1,6 @@
 import { select, update } from '~/db/query';
+
 export const mypage = async (req, res) => {
-  if (!req.user) {
-    res.redirect('/');
-  }
   const kakao_info = JSON.parse(req.session.passport.user._raw);
 
   const member_db = await select(
@@ -17,6 +15,7 @@ export const mypage = async (req, res) => {
     `author = ${member_db.rows[0].usernum}`,
   );
 
+  // 참여자
   const participants_db = await select(
     '*',
     'participants as p',
@@ -24,19 +23,36 @@ export const mypage = async (req, res) => {
     'LEFT JOIN public.member as m ON p.part_usernum = m.usernum',
   );
 
+  // 참여한 요청
+  const participation_db = await select(
+    'boardnum, title, author, locations, hospital, contents, show_flag',
+    'participants as p',
+    `p.part_usernum = ${member_db.rows[0].usernum} AND p.show_flag = '1'`,
+    'LEFT JOIN public.member as m ON p.part_usernum = m.usernum NATURAL JOIN public.board as b',
+  );
+
+  // 참여자 수 카운트
   let participants_count = 0;
   for (let iter in participants_db.rows) {
-    if (participants_db.rows[iter].show_flag) participants_count++;
+    if (participants_db.rows[iter].show_flag == '1') participants_count++;
   }
 
-  console.log(participants_db.rows);
+  // 참여 한 게시물 수 카운트
+  let participation_count = 0;
+  for (let iter in participation_db.rows) {
+    if (participation_db.rows[iter].show_flag == '1') participation_count++;
+  }
 
-  res.render('auth/mypage', {
+  console.log('참여 DB', participation_db.rows);
+  res.render('user/mypage', {
     kakao_info: kakao_info,
     member_db: member_db.rows[0],
     board_db: board_db.rows,
     participants: participants_db.rows,
+    participation: participation_db.rows,
     participants_count: participants_count,
+    participation_count: participation_count,
+    is_logedin: typeof req.session.passport === 'undefined' ? false : true,
   });
 };
 
