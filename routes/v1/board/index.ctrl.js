037fe250ce60e7a44844fec1b947858092f05a11
kyu <31264094+Q00@users.noreply.github.com>
2019-10-03 21:59:45 +0900

    Revert "Before release/notice"

--- routes/v1/board/index.ctrl.js
+++ routes/v1/board/index.ctrl.js
@@ -138,6 +138,7 @@ export const boardlist = async (req, res, next) => {
     const response = await axios.post(process.env.SLACK_BOT_ERROR_URL, {
       text: arr.join('\n'),
     });
+    console.log(err);
     next(err);
   }
 };
@@ -238,6 +239,7 @@ export const read = async (req, res, next) => {
     const response = await axios.post(process.env.SLACK_BOT_ERROR_URL, {
       text: arr.join('\n'),
     });
+    console.log(err);
     next(err);
   }
 };
@@ -298,6 +300,7 @@ export const upload = async (req, res, next) => {
     const response = await axios.post(process.env.SLACK_BOT_ERROR_URL, {
       text: arr.join('\n'),
     });
+    console.log(err);
     next(err);
   }
 };
@@ -343,6 +346,7 @@ export const comment_upload = async (req, res, next) => {
     const response = await axios.post(process.env.SLACK_BOT_ERROR_URL, {
       text: arr.join('\n'),
     });
+    console.log(err);
     next(err);
   }
 };
@@ -364,6 +368,7 @@ export const participate = async (req, res, next) => {
       text: arr.join('\n'),
     });
     next(err);
+    console.log(err);
   }
 
   res.redirect('back');
